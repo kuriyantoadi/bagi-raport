f945284df379d0705d296dfc7b7da2e8e89e1684
kuriyantoadi <melatikuning212@gmail.com>
2020-12-13 14:15:51 +0700

    13 update 2020, update 8

--- admin/header.php
+++ admin/header.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if ($_SESSION['status'] != "wali" && $_SESSION['status'] != "admin") {
+if ($_SESSION['status'] != "admin") {
   header("location:login.php?pesan=belum_login");
 }
 ?>
