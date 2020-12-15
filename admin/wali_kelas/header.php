9e1c0088a5b4248723cf376ef9eaf2f68041ccc8
kuriyantoadi <melatikuning212@gmail.com>
2020-12-15 16:12:01 +0700

    15 desember 2020, update 1

--- admin/wali_kelas/header.php
+++ admin/wali_kelas/header.php
@@ -21,7 +21,7 @@ while ($d = mysqli_fetch_array($data)) {
 <html lang="en">
 
 <head>
-    <title>Hasil Kelulusan Kelas XII AKL </title>
+    <title>Program Raport Wali Kelas </title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
